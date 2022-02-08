f31c16a4b430ad0af3430937f979d5b83d5b04c4
Javier Avalos <javalos18j2@gmail.com>
2022-02-07 20:27:01 -0600

    Creacion de la funcion para calcular los ingresos

--- js/app.js
+++ js/app.js
@@ -1,16 +1,23 @@
-
 const ingresos = [
-    new Ingreso("Salario", 3500),
-    new Ingreso("Apartamento", 2000),
+  new Ingreso("Salario", 3500),
+  new Ingreso("Apartamento", 2000),
 ];
 const egresos = [
-    new Egreso("Apartamento", 1800),
-    new Ingreso("Internet", 350),
-    new Ingreso("Universidad", 875),
+  new Egreso("Apartamento", 1800),
+  new Ingreso("Internet", 350),
+  new Ingreso("Universidad", 875),
 ];
 
-const cargarApp = () =>{
-    let cargarCabecero = () =>{
+const cargarApp = () => {
+  cargarCabecero();
+};
 
+let totalIngresos = () =>{
+    let totalIngreso = 0;
+    for (const ingreso of ingresos) {
+        totalIngreso += ingreso.valor;
     }
+    return totalIngreso;
 }
+
+let cargarCabecero = () => {};
