0727f6a681090cdc3ecc45c7c5de356b3d028d72
Javier Avalos <javalos18j2@gmail.com>
2022-02-07 20:33:30 -0600

    Creacion de las funciones para mostrar los datos que se estan obteniendo

--- js/app.js
+++ js/app.js
@@ -12,19 +12,26 @@ const cargarApp = () => {
   cargarCabecero();
 };
 
-let totalIngresos = () =>{
-    let totalIngreso = 0;
-    for (const ingreso of ingresos) {
-        totalIngreso += ingreso.valor;
-    }
-    return totalIngreso;
-}
-let totalEgresos = () =>{
-    let totalEgreso = 0;
-    for (const egreso of egresos) {
-        totalEgreso += egreso.valor;
-    }
-    return totalEgreso;
-}
+let totalIngresos = () => {
+  let totalIngreso = 0;
+  for (const ingreso of ingresos) {
+    totalIngreso += ingreso.valor;
+  }
+  return totalIngreso;
+};
+let totalEgresos = () => {
+  let totalEgreso = 0;
+  for (const egreso of egresos) {
+    totalEgreso += egreso.valor;
+  }
+  return totalEgreso;
+};
 
-let cargarCabecero = () => {};
+let cargarCabecero = () => {
+    let presupuesto = totalIngresos() - totalEgresos();
+    let porcentajeEgreso = totalIngresos()/totalEgresos();
+    document.getElementById("presupuesto").innerHTML = presupuesto;
+    document.getElementById("porcentaje").innerHTML = porcentajeEgreso;
+    document.getElementById("ingresos").innerHTML = totalIngresos();
+    document.getElementById("egresos").innerHTML = totalEgresos();
+};
