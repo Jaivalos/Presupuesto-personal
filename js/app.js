856138068a46cf27b0f69102625760bd3c87f7d2
Javier Avalos <javalos18j2@gmail.com>
2022-02-11 22:28:58 -0600

    Funcion de eliminar los egresos funcionando

--- js/app.js
+++ js/app.js
@@ -99,7 +99,8 @@ const crearEgresoHTML = (egreso) => {
        <div class="elemento_valor">-${formatoMoneda(egreso.valor)}</div>
        <div class="elemento_eliminar">
          <button class="elemento_eliminar--btn">
-           <ion-icon name="close-circle-outline"></ion-icon>
+           <ion-icon name="close-circle-outline"
+           onclick = eliminarEgreso(${egresos.id})></ion-icon>
          </button>
        </div>
      </div>
@@ -107,3 +108,9 @@ const crearEgresoHTML = (egreso) => {
   `;
   return egresoHTML;
 };
+
+const eliminarEgreso = (id) =>{
+  let indiceEliminar = egresos.findIndex(egreso => egreso.id === id);
+  egresos.splice(indiceEliminar, 1);
+  cargarCabecero(); cargarEgresos();
+}
