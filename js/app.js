d9474bdc80ecd0ba06d6ee20f924a9339f82033b
Javier Avalos <javalos18j2@gmail.com>
2022-02-12 21:39:43 -0600

    Creacion del selector para ingresos o egresos

--- js/app.js
+++ js/app.js
@@ -1,12 +1,5 @@
-const ingresos = [
-  new Ingreso("Salario", 3500),
-  new Ingreso("Apartamento", 2000),
-];
-const egresos = [
-  new Egreso("Apartamento", 1800),
-  new Ingreso("Internet", 350),
-  new Ingreso("Universidad", 875),
-];
+const ingresos = [];
+const egresos = [];
 
 const cargarApp = () => {
   cargarCabecero();
